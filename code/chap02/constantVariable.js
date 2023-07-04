b161a37a8ff51435a30ad77375921e13d33f4e20
Kim Ji Hun <gmldnr2222@naver.com>
2023-07-05 00:16:25 +0900

    feat: 02. 자료와 변수 / 02. 상수와 변수

---
+++ code/chap02/constantVariable.js
@@ -0,0 +1,62 @@
+// ### Identifier has already declared
+// const name = "name이라는 상수";
+// const name = "한 번 더 선언";
+
+// ### Missing initializer in constant declaration
+// const pi;
+
+// ### Assignment to constant variable
+// const name = "name이라는 상수";
+// name = "변경해 보자.";
+
+// 변수 선언 및 사용
+/**
+let pi = 3.141592
+console.log(pi);
+let r = 10;
+console.log(2 * pi * r)
+console.log(pi * r * r)
+*/
+
+// 변수의 값 변경
+/**
+let g = 9.8
+let m = 10
+console.log(g * m)
+g = 9.80665
+console.log(g * m)
+*/
+
+// ### Identifier has already been declared
+/*let name = "name 변수"
+let name = "한 번 더 선언"*/
+
+// 증감 연산자
+// let num = 10
+// num++
+// console.log(num)
+// ++num
+// console.log(num)
+
+// 증감 연산자 2
+/*let num = 10
+console.log(num++)
+console.log(num++)
+console.log(num++)*/
+
+// 후위 증감 풀어쓰기
+/*let num = 10
+console.log(num); num++
+console.log(num); num++
+console.log(num); num++*/
+
+// 전위 증감
+/*let num = 10
+console.log(++num)
+console.log(++num)
+console.log(++num)*/
+
+// #undefined
+console.log(typeof(abc))
+let a
+console.log(a)
