a9324a9e6e8363aed6876f3c2ec8659f49b2bf1c
Kim Ji Hun <gmldnr2222@naver.com>
2023-08-17 15:19:03 +0900

    feat: 09. 클래스 / 01. 클래스의 기본 기능

---
+++ code/chap09/01. startClass/class.js
@@ -0,0 +1,15 @@
+
+// 클래스를 선언함.
+class Student {
+
+}
+
+// 학생을 선언함.
+const student = new Student()
+
+const students = [
+    new Student(),
+    new Student(),
+    new Student(),
+    new Student()
+]
